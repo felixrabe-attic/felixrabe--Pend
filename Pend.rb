3ec83eea305a7fed04be486b8957b614f42c8cdd
Felix Rabe <public@felixrabe.net>
2012-05-08 21:41:00 +0200

    Task description

--- Pend.rb
+++ Pend.rb
@@ -1,6 +1,21 @@
 #!/usr/bin/env jruby
 # -*- coding: utf-8 -*-
 
+# German homework description:
+
+# Erstellen Sie eine Applikation zur Pendenzenverwaltung.
+
+# Eine Pendenz besteht aus einem Datum, einer Beschreibung und einem Flag, das
+# besagt, ob die Pendenz erledigt sei. Die Applikation soll in einer Tabelle die
+# Pendenzen zeigen. Man kann Pendenzen einfügen, löschen und editieren.
+
+# Verwenden Sie für das GUI eine JTable mit einem eigenen Tabellen-Model
+# (TableModel).
+
+# Entwerfen Sie die Applikation unter Berücksichtigung des MVC-Paradigmas.
+# Insbesondere sollte es später möglich sein, einzelne Bestandteile der
+# Applikation auszuwechseln.
+
 
 class Pend
 end
